8b29c644f5f3ae210c61b245d9ad9a444662a6e2
Axel Lopez <axel.aatl@gmail.com>
2025-07-05 14:45:12 -0600

    [ADD] App prod: Redis

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 echo "<h1>¡Hola desde PHP Demo!</h1>";
 echo "<p>Ambiente: PROD</p>";
+
+
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
+$redis->set("ct_app_prod", "Redis: PHP Demo PROD Environment");
+echo $redis->get("ct_app_prod");
+
 ?>
 
