5c5b83521cf5698616eb5e68515e0885bd2f0b78
Axel Lopez <axel.aatl@gmail.com>
2025-07-05 14:44:02 -0600

    [ADD] App dev: Redis

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 echo "<h1>¡Hola desde PHP Demo!</h1>";
 echo "<p>Ambiente: DEV</p>";
+
+
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
+$redis->set("ct_app_dev", "Redis: PHP Demo DEV Environment");
+echo $redis->get("ct_app_dev");
+
 ?>
 
